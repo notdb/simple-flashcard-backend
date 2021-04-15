7812e0964e90777970f29cdc487d8da9aca972a3
Doug <45773821+notdb@users.noreply.github.com>
2021-04-14 22:58:43 -0400

    small change

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const server = require("./api/server.js");
 
+
+// small test change
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
